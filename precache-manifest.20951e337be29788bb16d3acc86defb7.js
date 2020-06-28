89d9342ec60555bb16c28cc084e3732aa471ad08
kiran-nepali <cuziamnepali@hotmail.com>
2020-06-28 23:43:48 +0100

    Updates

---
+++ precache-manifest.20951e337be29788bb16d3acc86defb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67deb07a7d3ae579e97fd67503eadd39",
+    "url": "/React-FoodrecipeGraphQlHasura/index.html"
+  },
+  {
+    "revision": "c89cd285ab9c208e43df",
+    "url": "/React-FoodrecipeGraphQlHasura/static/css/main.fdc5cbe1.chunk.css"
+  },
+  {
+    "revision": "79b5e754b940721d0140",
+    "url": "/React-FoodrecipeGraphQlHasura/static/js/2.e17c7519.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/React-FoodrecipeGraphQlHasura/static/js/2.e17c7519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89cd285ab9c208e43df",
+    "url": "/React-FoodrecipeGraphQlHasura/static/js/main.730d5ce2.chunk.js"
+  },
+  {
+    "revision": "7b8fb52732fdeeef1837",
+    "url": "/React-FoodrecipeGraphQlHasura/static/js/runtime-main.b0d482cd.js"
+  }
+]);
